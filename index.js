0f742fc320642332de3e88f56da4a58a87df01f6
Jenn Soderborg <jsoderborg7@gmail.com>
2019-10-14 11:56:22 -0600

    started

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ server.get('/', (req, res) => {
 
 server.post('/api/register', (req, res) => {
   let user = req.body;
+  let hash = bcrypt.hashSync(user.password, 10);
+
+  user.password = hash;
 
   Users.add(user)
     .then(saved => {
@@ -34,7 +37,7 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -45,7 +48,7 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', protected, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -54,12 +57,19 @@ server.get('/api/users', (req, res) => {
 });
 
 server.get('/hash', (req, res) => {
-  let password = req.header;
-  let hash = bcrypt.hashSync(password)
+  let password = req.headers.authorization;
+  let hash = bcrypt.hashSync(password, 7);
+
+  res.status(200).json({hash});
   // read a password from the Authorization header
   // return an object with the password hashed using bcryptjs
   // { hash: '970(&(:OHKJHIY*HJKH(*^)*&YLKJBLKJGHIUGH(*P' }
 })
+
+function protected(){
+  let {username, password} = req.headers;
+  
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
